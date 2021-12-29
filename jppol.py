d09df36f56555ba13f2c6a33f064b4bea55e2625
Dinesh Narayan <dinesh@sourcepoint.com>
2021-12-29 19:04:04 +0000

    fourth commit - added original source code for jppol.py

--- jppol.py
+++ jppol.py
@@ -10,6 +10,7 @@ import sklearn as skl
 print('Python: {}'.format(sys.version))
 print('numpy: {}'.format(np.__version__))
 print('pandas: {}'.format(pd.__version__))
+#print('matplotlib: {}'.format(plt.__version__))
 print('scipy: {}'.format(scpy.__version__))
 print('sklearn: {}'.format(skl.__version__))
 
@@ -21,6 +22,7 @@ with open('query.sql', 'r') as f:
     query = f.read()
 cursor.execute(query)
 
+## 'fetchall()' method fetches all the rows from the last executed statement
 records = cursor.fetchall()
 df = pd.DataFrame(records)
 df.columns = [  "property_id", "views_date", "month_format", "views_property",
@@ -29,11 +31,22 @@ df.columns = [  "property_id", "views_date", "month_format", "views_property",
                 "users_legit_int_only", "users_legitimate_int_only_frac", "views_reject_only_frac", 
                 "users_reject_only_frac", "views_previously_opt_in_frac", "users_previously_opt_in_frac"    ]
 
+## showing one by one database
+## for record in records:
+##    print(record)
+
 cursor.close()
 cnx.close()
 
+## print(len(df['views_date']))
+
 plt.rcParams["figure.figsize"] = [7.50, 3.50]
 plt.rcParams["figure.autolayout"] = True
+# plt.plot(df.views_date, df.users_total_users, label="Total users")
+# plt.plot(df.views_date, df.views_total_views, label="Total views")
+# plt.xticks(df.views_date, rotation=90)
+# plt.yticks(df.users_total_users)
+# plt.show()
 
 # Initialise the subplot function using number of rows and columns
 figure, axis = plt.subplots(3, 1)
@@ -49,3 +62,16 @@ axis[2].plot(df.views_date, df.users_legitimate_int_only_frac, label="Fraction u
 axis[2].plot(df.views_date, df.views_legitimate_int_only_frac, label="Fraction views - legit interest")
 axis[2].set_title("Fraction Users vs. Fraction Views (Legit Interest only)")
 plt.show()
+
+
+
+# Python3 program to add two numbers
+# num1 = 15
+# num2 = 12  
+# Adding two nos
+# sum = num1 + num2
+# printing values
+# print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
+
+
+# %%
